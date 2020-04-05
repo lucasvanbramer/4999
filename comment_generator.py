64db44d4eca5733d9f64fcc54ea8032101880efa
Chenhao Tan <juliustch@gmail.com>
2020-04-05 07:36:01 -0700

    fix list

--- comment_generator.py
+++ comment_generator.py
@@ -3,7 +3,7 @@ from revision_pipeline import comments
 if __name__ == "__main__":
     # topics = ["Conversation",
     #          "Clique_(graph_theory)", "Cornell University", "Ithaca,_New_York"]
-    topics = "Conversation"
+    topics = ["Conversation"]
     cg = comments.CommentGenerator(topics)
     for el in cg.stream():
         print(el)
